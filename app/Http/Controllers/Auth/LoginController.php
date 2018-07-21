03fc756bfd682a7120b721a484f32cef67d7b9c6
surya.c <surya.c@krds.fr>
2018-07-21 10:48:25 +0530

    Configurations updated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,64 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function redirectToFacebook()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+	
+	public function handleGoogleCallback()
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+        } catch (Exception $e) {
+            return redirect('auth/google');
+        }   
+		$authUser = $this->createUser($user);
+		Auth::login($authUser, true);
+		return redirect()->route('home');        
+    }
+
+    public function handleFacebookCallback()
+    {
+        try {
+            $user = Socialite::driver('facebook')->user();
+        } catch (Exception $e) {
+            return redirect('auth/facebook');
+        }
+        $authUser = $this->createFBUser($user);
+        Auth::login($authUser, true);
+        return redirect()->route('home'); 
+    }
+	
+	public function createUser($user)
+    {
+        $authUser = User::where('google_id', $user->id)->first();
+		if($authUser){
+			return $authUser;
+		}
+		return User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'google_id' => $user->id,
+        ]);
+    }
+
+    public function createFBUser($user)
+    {
+        $authUser = User::where('facebook_id', $user->id)->first();
+		if($authUser){
+			return $authUser;
+		}
+		return User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'facebook_id' => $user->id,
+        ]);
+    }
 }
