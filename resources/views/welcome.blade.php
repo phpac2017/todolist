e8338c2760fd29670a08291242a74c449197ca7c
surya.c <surya.c@krds.fr>
2018-07-21 12:39:56 +0530

    Blades Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - ToDo List Application
                 </div>
 
                 <div class="links">
